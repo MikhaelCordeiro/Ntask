8a6898ac8c798d4a62d883299efec074d3cecc76
PR-KILLER <106992407+PR-KILLER@users.noreply.github.com>
2024-05-19 17:48:27 -0300

    .

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ import express from "express"
 
 const app = express();
 
+
+//ejs
+
+app.set('view engine', 'ejs')
+
+
 app.get('/', (req, res) => {
     res.render('index.ejs')
+})
+
+app.get('/NewTask', (req, res) => {
+    res.render('NovaTarefa.ejs')
 })
 
 app.listen(3000, () => console.log("O pai ta ON!!!"))
